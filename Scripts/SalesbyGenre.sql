9e6ab0db7d5a8a02b3938326f92247de518e1927
erika-e <erika.swartz@gmail.com>
2020-05-08 10:47:04 -0400

    Modify Group by and Select to Return Sales by Genre

--- Scripts/SalesbyGenre.sql
+++ Scripts/SalesbyGenre.sql
@@ -1,9 +1,8 @@
 SELECT 
-IL.Quantity*IL.UnitPrice AS SalesDollars
-,IL.TrackId
-,T.Name as TrackName 
-,G.Name as GendreName
+G.Name AS Genre
+,SUM(IL.Quantity*IL.UnitPrice) AS SalesDollars
 FROM InvoiceLine IL
 LEFT JOIN Track T on T.TrackId = IL.TrackId 
 LEFT JOIN Genre G on T.GenreId = G.GenreId 
-GROUP BY InvoiceId 
+GROUP BY G.Name
+ORDER BY SUM(IL.Quantity*IL.UnitPrice) DESC
