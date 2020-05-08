8befe48d6143938e431d2db5eeed8caf63173164
erika-e <erika.swartz@gmail.com>
2020-05-08 10:06:24 -0400

    Join Genre and Track Tables
    Desired query output is sales by genre

--- Scripts/SalesbyGenre.sql
+++ Scripts/SalesbyGenre.sql
@@ -1,5 +1,9 @@
 SELECT 
-Quantity*UnitPrice AS SalesDollars
-,TrackId
-FROM InvoiceLine
+IL.Quantity*IL.UnitPrice AS SalesDollars
+,IL.TrackId
+,T.Name as TrackName 
+,G.Name as GendreName
+FROM InvoiceLine IL
+LEFT JOIN Track T on T.TrackId = IL.TrackId 
+LEFT JOIN Genre G on T.GenreId = G.GenreId 
 GROUP BY InvoiceId 
